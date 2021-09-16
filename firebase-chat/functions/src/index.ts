2aa736e2d70fab660c17deca38307a8d0e48ac6f
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2021-09-16 09:37:59 -0300

    cloud functions implementation

--- firebase-chat/functions/src/index.ts
+++ firebase-chat/functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from "firebase-functions";
+import { user, UserRecord } from "firebase-functions/v1/auth";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,4 +7,8 @@ import * as functions from "firebase-functions";
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
+});
+
+export const createGoogleUser = functions.auth.user().onCreate((user: UserRecord) => {
+  console.log("USUARIO CRIADO");
 });
