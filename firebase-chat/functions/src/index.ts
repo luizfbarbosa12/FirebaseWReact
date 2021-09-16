499bce3ad0af2aae0d0f45147edb1b3c40f70159
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2021-09-16 13:01:15 -0300

    add profile pictures

--- firebase-chat/functions/src/index.ts
+++ firebase-chat/functions/src/index.ts
@@ -1,14 +1,21 @@
 import * as functions from "firebase-functions";
-import { UserRecord } from "firebase-functions/v1/auth";
+import * as admin from "firebase-admin";
+import {UserRecord} from "firebase-functions/v1/auth";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+admin.initializeApp();
 
-export const createGoogleUser = functions.auth.user().onCreate((user: UserRecord) => {
-      console.log("USUARIO CRIADO");
+export const createGoogleUser = functions.auth.user().
+    onCreate(async (user: UserRecord) => {
+      const googleProvider = user.providerData
+          .find((item) => item.providerId === "google.com");
+
+      if (googleProvider) {
+        await admin.firestore().collection("users").doc(user.uid).set({
+          name: user.displayName,
+          photoURL: user.photoURL,
+        });
+        console.log("Uhul deu certo");
+      } else {
+        console.log("não era google");
+      }
     });
